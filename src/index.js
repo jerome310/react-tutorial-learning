71b362465968f04c6b953396c01fd13d602ae9e5
jerome310 <jeromeyoung333@gmail.com>
2022-12-06 10:33:04 -0500

    Created a proper component

--- src/index.js
+++ src/index.js
@@ -1,8 +1,19 @@
 import React from 'react';
 import ReactDom from 'react-dom';
 
+// stateless functional component
+// always return JSX  
+
 function Greeting() {
-  return <h4>Hello Jsx my name is Jerome Young</h4>
+  return (
+    <div>
+      <h4>Hello Jsx my name is Jerome Young</h4>
+    </div>
+  )
 }
+
+// const Greeting = () => {
+//   return React.createElement('h1', {}, 'hello world');
+// }
 
 ReactDom.render(<Greeting/>, document.getElementById('root'));
