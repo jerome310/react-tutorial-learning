0afb2849c11932ced4c3fe6671bfe517064485e2
jerome310 <jeromeyoung333@gmail.com>
2022-12-17 12:58:50 -0500

    Nesting component code

--- src/index.js
+++ src/index.js
@@ -1,19 +1,39 @@
 import React from 'react';
 import ReactDom from 'react-dom';
+import './index.css';
 
 // stateless functional component
 // always return JSX  
 
-function Greeting() {
+function BookList() {
   return (
-    <div>
-      <h4>Hello Jsx my name is Jerome Young</h4>
-    </div>
+    <section className="booklist">
+      <Book/>
+      <Book/>
+      <Book/>
+      <Book/>
+    </section>
   )
 }
 
-// const Greeting = () => {
-//   return React.createElement('h1', {}, 'hello world');
-// }
+const Book = () => {
+ return <article className='book '>
+  <Image/>
+  <Title/>
+  <Author/>
+  </article>
+}
 
-ReactDom.render(<Greeting/>, document.getElementById('root'));
+const Image = () => {
+ return <img src="https://m.media-amazon.com/images/W/WEBP_402378-T1/images/I/51p2SDOCV9L._SX482_BO1,204,203,200_.jpg" alt="" />
+}
+
+const Title = () => {
+ return <h1>I Love You to the Moon and Back</h1>
+}
+
+const Author = () => {
+ return <h4>Amelia Hepworth</h4>
+}
+
+ReactDom.render(<BookList/>, document.getElementById('root'));
