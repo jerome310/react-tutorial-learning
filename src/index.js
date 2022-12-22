677d8302f38e26bc1a2d10de259f2bde61f91408
jerome310 <jeromeyoung333@gmail.com>
2022-12-22 00:41:06 -0500

    learned how to iterate through data for a component

--- src/index.js
+++ src/index.js
@@ -1,38 +1,46 @@
-import React from 'react';
-import ReactDom from 'react-dom';
-import './index.css';
+import React from "react";
+import ReactDom from "react-dom";
+import "./index.css";
 
 // set variables
-const firstBook = {
-  img: 'https://m.media-amazon.com/images/W/WEBP_402378-T1/images/I/51p2SDOCV9L._SX482_BO1,204,203,200_.jpg',
-  title: 'I Love You to the Moon and Back',
-  author: 'Amelia Hepworth'
-}
+const books = [
+  {
+    img: "https://m.media-amazon.com/images/W/WEBP_402378-T1/images/I/51p2SDOCV9L._SX482_BO1,204,203,200_.jpg",
+    title: "I Love You to the Moon and Back",
+    author: "Amelia Hepworth",
+  },
 
-const secondBook = {
-  img: 'https://m.media-amazon.com/images/W/WEBP_402378-T1/images/I/510g8NLbpNL._SX218_BO1,204,203,200_QL40_FMwebp_.jpg',
-  title: 'Our Class is a Family',
-  author: 'Shannon Olsen'
-}
+  {
+    img: "https://m.media-amazon.com/images/W/WEBP_402378-T1/images/I/510g8NLbpNL._SX218_BO1,204,203,200_QL40_FMwebp_.jpg",
+    title: "Our Class is a Family",
+    author: "Shannon Olsen",
+  },
+];
 
 function BookList() {
   return (
     <section className="booklist">
-      <Book img={firstBook.img} title={firstBook.title} author={firstBook.author}/>
-      <Book img={secondBook.img} title={secondBook.title} author={secondBook.author}/>
+      {books.map((book) => {
+        return (
+          <div>
+            <img src={book.img} alt="" />
+            <h4>{book.title}</h4>
+            <p>{book.author}</p>
+          </div>
+        );
+      })}
     </section>
-  )
+  );
 }
 
+// const Book = ({img, title, author}) => {
+//  return (
+//   <article className='book'>
+//   <img src={img} alt="" />
+//   <h1>{title}</h1>
+//   <h4>{author}</h4>
+//   </article>
+//  )
+// }
 
-const Book = ({img, title, author}) => {
- return (
-  <article className='book'>
-  <img src={img} alt="" />
-  <h1>{title}</h1>
-  <h4>{author}</h4>
-  </article>
- )
-}
-
-ReactDom.render(<BookList/>, document.getElementById('root'));
+ReactDom.render(<BookList />, document.getElementById("root"));
